1d4fd560e28e7b1448f78bfc068c3dfe1be67f70
Amauri1327 <amauri1816@hotmail.com>
2024-11-25 21:26:57 -0300

    adicao CrossOrigin permitir acesso do front-End local

--- src/main/java/Cetas/resgate/Resources/ResgateResource.java
+++ src/main/java/Cetas/resgate/Resources/ResgateResource.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/resgates")
+@CrossOrigin(origins = "http://localhost:5173") //Permite o acesso apenas do frontend
 public class ResgateResource {
 
     @Autowired
